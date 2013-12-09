05568e0555c1e709b4f4a0461784ed6681ec4daa
qguantwer <qguan@thoughtworks.com>
2013-12-09 18:13:25 +0800

    add build clean

--- CI/jenkins_test_Build.sh
+++ CI/jenkins_test_Build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-xcodebuild test -scheme jenkins -destination OS=7.0,"name=iPhone Retina (4-inch 64-bit)" -project ./jenkins/jenkins.xcodeproj | /Users/twer/Documents/ocunit2junit
+xcodebuild clean -project ./*/*.xcodeproj
+xcodebuild test -scheme jenkins -destination OS=7.0,"name=iPhone Retina (4-inch 64-bit)" -project ./*/*.xcodeproj | /Users/twer/Documents/ocunit2junit
