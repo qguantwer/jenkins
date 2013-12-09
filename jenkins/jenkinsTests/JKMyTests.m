fe9282693348354a3862b8051dbbe8a3830576be
qguantwer <qguan@thoughtworks.com>
2013-12-09 14:09:56 +0800

    add my test and remove the useless code.

---
+++ jenkins/jenkinsTests/JKMyTests.m
@@ -0,0 +1,34 @@
+//
+//  JKMyTests.m
+//  jenkins
+//
+//  Created by qguan on 12/9/13.
+//  Copyright (c) 2013 qguan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JKMyTests : XCTestCase
+
+@end
+
+@implementation JKMyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+
+}
+
+@end
