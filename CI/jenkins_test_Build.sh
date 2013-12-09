bd8bf777e66482a5e06bfbc1aa34ebb001fd5eb5
qguantwer <qguan@thoughtworks.com>
2013-12-09 16:33:06 +0800

    add multi device

--- CI/jenkins_test_Build.sh
+++ CI/jenkins_test_Build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 xcodebuild test -scheme jenkins -destination OS=7.0,"name=iPhone Retina (4-inch 64-bit)" -project ./jenkins/jenkins.xcodeproj | /Users/twer/Documents/ocunit2junit
+xcodebuild test -scheme jenkins -destination OS=7.0,"name=iPhone Retina (4-inch)" -project ./jenkins/jenkins.xcodeproj | /Users/twer/Documents/ocunit2junit
+xcodebuild test -scheme jenkins -destination OS=7.0,"name=iPhone Retina (3.5-inch)" -project ./jenkins/jenkins.xcodeproj | /Users/twer/Documents/ocunit2junit
