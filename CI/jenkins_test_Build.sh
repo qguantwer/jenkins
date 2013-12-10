71ac964531c43433a768b0ca64be6ff746d65804
qguantwer <qguan@thoughtworks.com>
2013-12-10 19:01:56 +0800

    code coverage - generate html and present

--- CI/jenkins_test_Build.sh
+++ CI/jenkins_test_Build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xcodebuild clean -project ./*/*.xcodeproj
-xcodebuild test -scheme jenkins -destination OS=7.0,"name=iPhone Retina (4-inch 64-bit)" -project ./*/*.xcodeproj | /Users/twer/Documents/ocunit2junit
+xcodebuild clean -project ./jenkins/jenkins.xcodeproj
+xcodebuild test -scheme jenkins -destination OS=7.0,"name=iPhone Retina (4-inch 64-bit)" -project ./jenkins/jenkins.xcodeproj | /Users/twer/Documents/ocunit2junit
