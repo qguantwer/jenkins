7d54cd6e246fbbe5aaae293f3237d971c8b4995d
qguantwer <qguan@thoughtworks.com>
2013-12-18 17:13:35 +0800

    add OCMock to support stub and mock. add example for stub and mock. OCMock lib has problem on 64bit x86_64 device.

--- jenkins/jenkinsTests/JKMyTests.m
+++ jenkins/jenkinsTests/JKMyTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "OCMock.h"
 
 @interface JKMyTests : XCTestCase
 
@@ -29,6 +30,13 @@
 - (void)testExample
 {
     XCTAssertTrue(YES, @"Test assert true.");
+    id arrayMock = [OCMockObject mockForClass:[NSArray class]];
+    [[[arrayMock stub] andReturn:@"test stub"] lastObject];
+    XCTAssert([[arrayMock lastObject] isEqualToString:@"test stub"], @"test stub");
+    
+    [[arrayMock expect] isEqualToArray:@[@"test stub"]];
+    [arrayMock isEqualToArray:@[@"test stub"]];
+    [arrayMock verify];
 }
 
 @end
